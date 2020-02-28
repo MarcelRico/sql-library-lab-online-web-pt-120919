fd29020e4c80427f721d242913820bc114aac6f3
Marcel Rico <personal.mr4@gmail.com>
2020-02-28 22:40:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT characters FROM series JOIN characters ON series.author_id = characters.author_id
-  WHERE characters.species = 'Human'";
+  WHERE characters.species = 'Human' GROUP BY series.title;";
 end
 
 def select_character_names_and_number_of_books_they_are_in
